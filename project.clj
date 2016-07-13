b5807a44251ddfb3ae17a8f724b232b7ef4c9e7e
Paul Landes <landes@mailc.net>
2016-07-13 16:30:02 -0500

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.zensols.tools/actioncli "0.0.3"
+(defproject com.zensols.tools/actioncli "0.0.4"
   :description "An action oriented framework to the CLI (and various other libraries)."
   :url "https://github.com/plandes/clj-actionclj"
   :license {:name "Apache License version 2.0"

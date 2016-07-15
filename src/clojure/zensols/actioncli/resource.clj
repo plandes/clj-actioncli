4beea36bd4a7ebcfa4074206ea493a0862b61b03
Paul Landes <landes@mailc.net>
2016-07-15 10:52:51 -0500

    doc

--- src/clojure/zensols/actioncli/resource.clj
+++ src/clojure/zensols/actioncli/resource.clj
@@ -34,8 +34,8 @@
 
   ## Keys
 
-  * **:create?** if `true` then create the resource (i.e. director(ies) on the
-  file system)"
+  * **:create?** if `:file` then create the director(ies) on the
+  file system, otherwise if `:dir` then create all parent directories"
   ([key child-file & {:keys [create?] :or {:create? nil}}]
    (let [path (io/file (resource-path key) child-file)]
      (case create?

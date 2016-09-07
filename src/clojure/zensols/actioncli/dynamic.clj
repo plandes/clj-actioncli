d8e131df872c4b775c849f0c7418cff8466abe49
Paul Landes <landes@mailc.net>
2016-09-07 15:51:35 -0500

    convenience macros

--- src/clojure/zensols/actioncli/dynamic.clj
+++ src/clojure/zensols/actioncli/dynamic.clj
@@ -22,10 +22,12 @@ See [[purge]]."
 (defn dyn-init-var
   "Create a variable binding if it isn't already defined.
 
+  You probably want to use [[def-noclob]] and [[def-atom]] over this.
+
 Usage:
 ```clojure
 (dyn-init-var *ns* 'some-data-var (atom nil))
-;(ns-unmap *ns* 'some-data-varl)
+;(ns-unmap *ns* 'some-data-var)
 ```
 
   The second commented form is useful to *invalidate* a variable so a subsequent
@@ -41,3 +43,33 @@ Usage:
      (when-not (resolve var-sym)
        (let [new-var (intern *ns* var-sym)]
          (alter-var-root new-var (constantly init-val)))))))
+
+(defmacro def-noclob
+  "Create a variable binding out of **var** that doesn't already exist.  The
+  variable is initialized to **init-value**, otherwise it is bound to `nil`.
+
+  See [[def-atom]] and [[undef]]."
+  ([var]
+   `(dyn-init-var *ns* (quote ~var)))
+  ([var init-value]
+   `(dyn-init-var *ns* (quote ~var) (quote ~init-value))))
+
+(defmacro def-atom
+  "Just like [[def-noclob]] but the initialization value is an atom created
+  with **init-value** or `nil` if not given.
+
+  See [[def-noclob]] and [[undef]]."
+  ([var]
+   `(dyn-init-var *ns* (quote ~var) (atom nil)))
+  ([var init-value]
+   `(dyn-init-var *ns* (quote ~var) (atom (quote ~init-value)))))
+
+(defmacro undef
+  "Unbind a variable in the current namespace.  Just like:
+```
+(ns-unmap *ns* 'var)
+```
+
+  See [[def-noclob]]"
+  [var]
+  `(ns-unmap *ns* (quote ~var)))

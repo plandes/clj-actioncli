2c347b154bce195ca23fd642f766061b1a112367
Paul Landes <landes@mailc.net>
2016-12-11 22:41:05 -0600

    inc

--- project.clj
+++ project.clj
@@ -26,11 +26,11 @@
 
                  ;; logging
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.logging.log4j/log4j-api "2.7"]
                  [org.apache.logging.log4j/log4j-core "2.7"]]
   :profiles {:appassem {:aot :all}
              :dev
              {:jvm-opts ["-Xms4g" "-Xmx12g" "-XX:+UseConcMarkSweepGC"]
-              :dependencies [[org.apache.logging.log4j/log4j-api "2.7"]
-                             [org.apache.logging.log4j/log4j-slf4j-impl "2.7"]
+              :dependencies [[org.apache.logging.log4j/log4j-slf4j-impl "2.7"]
                              [org.apache.logging.log4j/log4j-jcl "2.7"]
                              [com.zensols/clj-append "1.0.4"]]}})

64e60a5210c8853178bd45940383ad5e60449661
Paul Landes <landes@mailc.net>
2016-12-13 15:29:19 -0600

    log

--- src/clojure/zensols/actioncli/log4j2.clj
+++ src/clojure/zensols/actioncli/log4j2.clj
@@ -18,6 +18,7 @@
 
   **level-thing** the log level as either the Level instance or a string."
   [level-thing]
+  (log/debugf "new log level: %s" level-thing)
   (let [level (if (string? level-thing)
                 (Level/toLevel level-thing)
                 level-thing)]
@@ -26,6 +27,7 @@
 (defn configure
   "Congigure the Log4j2 system with an XML resource."
   [xml-resource]
+  (log/debugf "configuring with XML resource: %s" xml-resource)
   (let [res (io/resource xml-resource)]
     (if (nil? res)
       (throw (ex-info (format "No such resource: %s" xml-resource)
@@ -39,8 +41,8 @@
   ([short long]
    [short long "Log level to set in the Log4J2 system."
     :required "LOG LEVEL"
-    :default (org.apache.logging.log4j.Level/toLevel "info")
-    :parse-fn #(org.apache.logging.log4j.Level/toLevel % nil)
+    :default (Level/toLevel "info")
+    :parse-fn #(Level/toLevel % nil)
     :validate [(fn [level]
                  (when level
                    (change-log-level level)

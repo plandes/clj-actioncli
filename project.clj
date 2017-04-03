239f72b767d1c93d0e98620f20dd9aeff4f0479c
Paul Landes <landes@mailc.net>
2017-04-03 00:41:59 -0500

    move to lein-git 1.2.7

--- project.clj
+++ project.clj
@@ -6,10 +6,13 @@
             :distribution :repo}
   :plugins [[lein-codox "0.10.1"]
             [lein-javadoc "0.3.0"]
-            [org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
+            [org.clojars.cvillecsteele/lein-git-version "1.2.7"]]
   :codox {:metadata {:doc/format :markdown}
           :project {:name "Action CLI"}
           :output-path "target/doc/codox"}
+  :git-version {:root-ns "zensols.actioncli"
+                :path "src/clojure/zensols/actioncli"
+                :version-cmd "git describe --match v*.* --abbrev=4 --dirty=-dirty"}
   :javadoc-opts {:package-names ["com.zensols.log"]
                  :output-dir "target/doc/apidocs"}
   :source-paths ["src/clojure"]
@@ -28,6 +31,7 @@
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:provided {:dependencies
                         [[org.apache.logging.log4j/log4j-core "2.7"]]}
+             :snapshot {:git-version {:version-cmd "echo -snapshot"}}
              :appassem {:aot :all}
              :dev
              {:jvm-opts ["-Dlog4j.configurationFile=test-resources/log4j2.xml" "-Xms4g" "-Xmx12g" "-XX:+UseConcMarkSweepGC"]

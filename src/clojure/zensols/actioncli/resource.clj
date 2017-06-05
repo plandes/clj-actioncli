6e7c9ba95fd3570aa69984663cd62eee680eac1a
Paul Landes <landes@mailc.net>
2017-06-05 18:36:52 -0500

    better function resource type handling; more testing

--- src/clojure/zensols/actioncli/resource.clj
+++ src/clojure/zensols/actioncli/resource.clj
@@ -4,11 +4,25 @@
   (:import [java.io File]
            [java.net URL])
   (:require [clojure.tools.logging :as log]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.test :refer (function?)]))
 
 (def ^:private our-ns *ns*)
+
 (def ^:private res-prop-fmt (atom "zensols.%s"))
-(def ^:private res-paths (atom {}))
+
+(defonce ^{:dynamic true
+           :doc "Meant for this namespace internal use only."}
+  *resource-paths*
+  (atom {}))
+
+(defmacro with-resources
+  "Allows for temporal registration and path rendering in a lexical context."
+  {:style/indent 0}
+  [& forms]
+  `(let [curr-res-paths# (deref *resource-paths*)]
+     (binding [*resource-paths* (atom curr-res-paths#)]
+       ~@forms)))
 
 (defn- sysprop [name & {:keys [throw?] :or {throw? true}}]
   (let [val (System/getProperty (format @res-prop-fmt name))]
@@ -43,6 +57,7 @@
                        (case restype
                          :file (.getFile prepath)
                          :resource (.toString prepath))
+
                        true prepath)
          catpath (if path
                    (str prepath "/" path)
@@ -59,6 +74,7 @@
    (resolve-resource type prepath nil))
   ([type prepath path]
    (case type
+     :function (prepath path)
      :file (if (instance? File prepath)
              (if path
                (io/file prepath path)
@@ -66,7 +82,17 @@
              (if path
                (io/file (resource-to-path :file prepath nil) path)
                (io/file (resource-to-path :file prepath nil))))
-     :resource (resource-to-path :resource prepath path))))
+     :resource (resource-to-path :resource prepath path)
+     :constant prepath)))
+
+(defn- resolve-function [key apply?]
+  (let [path-fn (get (deref *resource-paths*) key)]
+    (if (nil? path-fn)
+      (throw (ex-info (format "No such resource: %s" key) {:key key})))
+    (let [path-or-fn (path-fn)]
+      (if (and apply? (function? path-or-fn))
+        (path-or-fn)
+        path-or-fn))))
 
 (defn resource-path
   "Get a path to a resource.
@@ -77,8 +103,10 @@
   * **:create** if `:file` then create the director(ies) on the
   file system, otherwise if `:dir` then create all parent directories"
   ([key child-file & {:keys [create] :or {:create nil}}]
-   (let [parent (resource-path key)
-         path (resolve-resource (if (instance? File parent) :file :resource)
+   (let [parent (resolve-function key false)
+         path (resolve-resource (cond (instance? File parent) :file
+                                      (function? parent) :function
+                                      true :resource)
                                 parent child-file)]
      (case create
        :file (.mkdirs path)
@@ -86,10 +114,7 @@
        :else)
      path))
   ([key]
-   (let [path-fn (get @res-paths key)]
-     (if (nil? path-fn)
-       (throw (ex-info (format "No such resource: %s" key) {:key key})))
-     (path-fn))))
+   (resolve-function key true)))
 
 (defn set-resource-property-format
   "The format string (used with `format`) to generate the system property for
@@ -107,7 +132,10 @@
   All keys are optional but at least :function or :system-file must be
   provided.
 
-  * **:function** the function to call when the resource is generated
+  * **:function** the function to call when the resource is generated (see
+  the [resource
+  test](https://github.com/plandes/clj-actioncli/blob/master/test/zensols/actioncli/resource_test.clj)
+  and example of how to use this)
   * **:type** the type of resource to return; one of `:resource` or `:file` (default)
   * **:pre-path** a key of a resource to prepend to the path
   * **:system-file** a file path of the resource--if the same name exists as a
@@ -118,7 +146,9 @@
   [key & {:keys [function pre-path constant type
                  system-file system-property system-default]
           :or {type :file}}]
-  (let [fnform (concat (list (or function 'resolve-resource) type)
+  (let [fnform (concat (if function
+                         [function type]
+                         ['resolve-resource type])
                        (if pre-path
                          (list `(resource-path ~pre-path)))
                        (if constant
@@ -132,6 +162,6 @@
         ;; bind since calling from other *ns* fails the eval (i.e. requires)
         fval (binding [*ns* our-ns]
                (eval (concat (list 'fn [] fnform))))]
-    (swap! res-paths assoc key fval)
+    (swap! *resource-paths* assoc key fval)
     (log/tracef "resource-path: %s -> %s" key (resource-path key))
     fval))
--- test/zensols/actioncli/resource_test.clj
+++ test/zensols/actioncli/resource_test.clj
@@ -4,14 +4,18 @@
             [clojure.string :as s])
   (:require [zensols.actioncli.resource :refer :all]))
 
-(deftest test-register
-  (testing "register"
-    (is (function? (register-resource :data :system-file "data" :system-default "../data")))
-    (is (function? (register-resource :runtime-gen :pre-path :data :system-file "db")))
-    (is (function? (register-resource :root-pkg :constant "clojure" :type :resource)))))
+(defn- init-sysprop []
+  ;; multiple runs
+  (System/clearProperty "zensols.data"))
+
+(defn- init-register []
+  (register-resource :data :system-file "data" :system-default "../data")
+  (register-resource :runtime-gen :pre-path :data :system-file "db")
+  (register-resource :root-pkg :constant "clojure" :type :resource))
 
 (deftest test-resource
-  (System/clearProperty "zensols.data") ; multiple runs
+  (init-sysprop)
+  (init-register)
   (testing "resource path"
     (is (= "../data" (.getPath (resource-path :data))))
     (is (= "../data/db" (.getPath (resource-path :runtime-gen)))))
@@ -21,16 +25,55 @@
     (is (= "/new-data-path/db" (.getPath (resource-path :runtime-gen))))))
 
 (deftest test-java-resource
-  (register-resource :data :system-file "data" :system-default "../data")
-  (register-resource :runtime-gen :pre-path :data :system-file "db")
+  (init-sysprop)
   (register-resource :root-pkg :constant "clojure" :type :resource)
   (is (not (nil? (resource-path :root-pkg))))
   (is (s/starts-with? (.toString (resource-path :root-pkg)) "jar:file:"))
   (is (s/ends-with? (.toString (resource-path :root-pkg))
-                                        "org/clojure/clojure/1.8.0/clojure-1.8.0.jar!/clojure"))
+                    "org/clojure/clojure/1.8.0/clojure-1.8.0.jar!/clojure"))
   (is (s/starts-with? (.toString (resource-path :root-pkg "string.clj")) "jar:file:"))
   (is (s/ends-with? (.toString (resource-path :root-pkg "string.clj"))
                     "org/clojure/clojure/1.8.0/clojure-1.8.0.jar!/clojure/string.clj"))
   (is (let [content (with-open [reader (io/reader (resource-path :root-pkg "string.clj"))]
                       (slurp reader))]
         (> (count content) 100))))
+
+(deftest test-prepath
+  (init-sysprop)
+  (register-resource :data :system-file "data" :system-default "../data")
+  (register-resource :runtime-gen :pre-path :data :system-file "db")
+  (is (not (nil? (resource-path :runtime-gen))))
+  (is (instance? java.io.File (resource-path :runtime-gen)))
+  (is (= "../data/db/dir2" (.getPath (resource-path :runtime-gen "dir2"))))
+  (System/setProperty "zensols.data" "/new-data-path")
+  (is (= "/new-data-path/db/dir3" (.getPath (resource-path :runtime-gen "dir3")))))
+
+(deftest test-func
+  (init-sysprop)
+  (register-resource :func-dir
+                     :type :any-old-key
+                     :function (fn [type-param]
+                                 (fn
+                                   ([] "/nofile")
+                                   ([file]
+                                    [type-param (str "/somepath/" file)]))))
+  (is (= "/nofile" (resource-path :func-dir)))
+  (is (= [:any-old-key "/somepath/pos"] (resource-path :func-dir "pos")))
+  (register-resource :func-dir
+                     :function (fn [arg]
+                                 (fn
+                                   ([] (io/file "no-file-for-you"))
+                                   ([file]
+                                    (io/file "/another/path" file)))))
+  (is (= (io/file "/another/path/pos") (resource-path :func-dir "pos")))
+  (is (= (io/file "no-file-for-you") (resource-path :func-dir))))
+
+(deftest test-with-resources
+  (init-sysprop)
+  (register-resource :data :system-file "../more-data")
+  (register-resource :runtime-gen :pre-path :data :system-file "db")
+  (is (= (io/file "../more-data/db") (resource-path :runtime-gen)))
+  (with-resources
+    (register-resource :data :system-file "../less-data")
+    (is (= (io/file "../less-data/db") (resource-path :runtime-gen))))
+  (is (= (io/file "../more-data/db") (resource-path :runtime-gen))))

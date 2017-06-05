f3ce4cee1666e3999b2cbac5b561d15eb8b8b153
Paul Landes <landes@mailc.net>
2017-06-05 13:46:34 -0500

    add log settable log level per package

--- src/clojure/zensols/actioncli/log4j2.clj
+++ src/clojure/zensols/actioncli/log4j2.clj
@@ -17,27 +17,29 @@
   "Default log level for [[log-level-set-option]]."
   (Level/toLevel "info"))
 
+(defn- to-level [level-thing]
+  (if (string? level-thing)
+    (Level/toLevel level-thing)
+    level-thing))
+
 (defn change-log-level
   "Change the Log4j2 log level.
 
   **level-thing** the log level as either the Level instance or a string."
-  [level-thing]
-  (log/debugf "new log level: %s" level-thing)
-  (let [level (if (string? level-thing)
-                (Level/toLevel level-thing)
-                level-thing)]
-    (LogUtil/setAllLevel level)))
+  ([log-name level-thing]
+   (log/debugf "new log level: %s -> %s" log-name level-thing)
+   (LogUtil/setLevel log-name (to-level level-thing)))
+  ([level-thing]
+   (log/debugf "new log level: %s" level-thing)
+   (LogUtil/setAllLevel (to-level level-thing))))
 
 (defn configure
-  "Congigure the Log4j2 system with an XML resource."
-  [xml-resource]
-  (let [res (io/resource xml-resource)]
-    (if (nil? res)
-      (throw (ex-info (format "No such resource: %s" xml-resource)
-                      {:xml-resource xml-resource})))
-    (with-open [in (io/input-stream res)]
-      (LogUtil/config in))
-    (log/debugf "configured with XML resource: %s" xml-resource)))
+  "Congigure the Log4j2 system with **res**, which can be anything usable
+  with [[clojure.java.io/input-stream]]."
+  [res]
+  (with-open [in (io/input-stream res)]
+    (LogUtil/config in))
+  (log/debugf "configured with XML resource: %s" res))
 
 (defn log-level-set-option
   "Create an option that sets the Log4j2 level.  Note that all you need to do

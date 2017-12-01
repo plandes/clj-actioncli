0e7c1b5abb9685d7f3d489b74d91fbd3b8c8c355
Paul Landes <landes@mailc.net>
2017-12-01 13:22:32 -0600

    none

--- src/clojure/zensols/actioncli/ns.clj
+++ src/clojure/zensols/actioncli/ns.clj
@@ -4,7 +4,7 @@ Namespace macros taken from
 [clojure/clojure-contrib](https://clojure.github.io/clojure-contrib/with-ns-api.html)."}
     zensols.actioncli.ns)
 
-(defmacro with-ns
+(defmacro with-other-ns
   "Evaluates body in another namespace.  ns is either a namespace
   object or a symbol.  This makes it possible to define functions in
   namespaces other than the current one."
@@ -18,8 +18,30 @@ Namespace macros taken from
   [& body]
   `(try
     (create-ns 'sym#)
-    (let [result# (with-ns 'sym#
+    (let [result# (with-other-ns 'sym#
                     (clojure.core/refer-clojure)
+                    ~@body)]
+      result#)
+    (finally (remove-ns 'sym#))))
+
+(defmacro with-ns
+  "Just like [[with-temp-ns]] but allows an [[clojure.core/ns]]
+  specification (not including the namespace name).
+
+  ## Example
+
+  ```
+  (with-declare-ns
+    [(:require [clojure.string :as s])]
+    (s/capitalize \"here\"))
+  ```"
+  {:style/indent 1}
+  [ns-args & body]
+  `(try
+    (create-ns 'sym#)
+    (let [result# (with-other-ns 'sym#
+                    (clojure.core/refer-clojure)
+                    (ns sym# ~@ns-args)
                     ~@body)]
       result#)
     (finally (remove-ns 'sym#))))
--- test/zensols/actioncli/namespace_test.clj
+++ test/zensols/actioncli/namespace_test.clj
@@ -4,11 +4,15 @@
 
 (deftest test-resource
   (testing "invoke via existing ns"
-    (with-ns 'clojure.string
+    (with-other-ns 'clojure.string
       (clojure.test/is (= "Dog" (capitalize "dog")))))
   (testing "temporary namespace"
     (with-temp-ns 'some.non.existing.ns
       (def some-new-var 'aval)
       (clojure.test/is (= "Dog" (clojure.string/capitalize "dog")))
       (clojure.test/is (resolve 'some-new-var)))
-    (is (not (resolve 'some-new-var)))))
+    (is (not (resolve 'some-new-var))))
+  (testing "requires in ns"
+    (is (= "Doc"
+           (with-ns [(:require [clojure.string :as s])]
+             (s/capitalize "doc"))))))
